d503ac866434f422a7b129e09792363eeb0e4c8c
Alekro <oleksiikoleshyn@smartoakprojects.com>
2026-07-03 23:03:26 +0200

    enviroment

--- src/config/container.ts
+++ src/config/container.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from "../api/HttpClient";
 import { GenerationService } from "../services/GenerationService";
 
-const httpClient = new HttpClient("http://localhost:8080");
+const httpClient = new HttpClient(import.meta.env.VITE_API_URL);
 export const generationService = new GenerationService(httpClient);
